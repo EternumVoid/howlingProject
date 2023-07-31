1da28a46c3ac7f3407e912b54b1530ce73c98736
Void Alex <alexmvoid@gmail.com>
2023-07-31 19:27:37 +0300

    Last one, maybe ?

---
+++ howlingProject/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
+
+
+# custom file so i can overwrite the UserCreationForm and implement the 'category' field
+
+class CustomUserCreationForm(UserCreationForm):
+    category = forms.ModelChoiceField(queryset=Group.objects.all())
+    profile_picture = forms.ImageField(required=False)
+
+    class Meta:
+        model = User  # Replace with your custom user model
+        # fields = UserCreationForm.Meta.fields + ('category',)
+        fields = UserCreationForm.Meta.fields + ('category', 'profile_picture',)
